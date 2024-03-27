80108d200787f58125fa6374ffb4801530e9102a
Dawithabte31 <dawithabitamu31@gmail.com>
2024-03-27 15:33:24 +0300

    changes to post

--- blog-frontnd/src/components/Post.tsx
+++ blog-frontnd/src/components/Post.tsx
@@ -1,9 +1,6 @@
 
 import  { NavLink } from 'react-router-dom'
 
-
-
-
 interface Category {
   name: string;
 }
@@ -16,6 +13,7 @@ interface Props {
     _id:any;
     createdAt: string;
     desc: string;
+    username:string;
   };
 }
 
@@ -23,24 +21,24 @@ const Post: React.FC<Props> = ({ post }) => {
   
   const PF="http://localhost:5000/images/";
 
-  console.log(post.photo)
   return (
-    <div className='Post'>
+    <div className='Post mx-1'>
 {post.photo && <img src={PF + post.photo} alt='' />}      
 <div className='Post-Info'>
         <div className='PostCats'>
           {post.categories.map((cat,index) => (
-
             <span key={index} >{cat.name}</span>
-            
           ))}
         </div>
         <NavLink style={{textDecoration:"none",color:"black"}} to={`/post/${post._id}`}>
           <span className='postTitle'>{post.title}</span>
         </NavLink>
-        <span className='postDate'>{new Date(post.createdAt).toDateString()}</span>
+        <div className=''>      
+          <span className='spanuser text-start mx-4 font-medium'>{post.username}</span>  
+          <span className='postDate text-end mx-10'>{new Date(post.createdAt).toDateString()}</span>
+        </div>
       </div>
-      <p className='postDesc'>{post.desc}</p>
+      <p className='postDesc text-start'>{post.desc}</p>
     </div>
   );
 };
